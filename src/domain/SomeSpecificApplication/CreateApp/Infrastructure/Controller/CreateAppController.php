d43d5f31ac62c3f957b292ef77893f7e60072bcb
tyamahori <tyamahroi@gmail.com>
2021-12-20 01:09:19 +0900

    fix: コントローラーの具象クラスを移動させる

--- src/domain/SomeSpecificApplication/CreateApp/Infrastructure/Controller/CreateAppController.php
+++ src/domain/SomeSpecificApplication/CreateApp/Infrastructure/Controller/CreateAppController.php
@@ -1,36 +1,43 @@
 <?php
 
-namespace Domain\SomeSpecificApplication\CreateApp\Controller;
+namespace Domain\SomeSpecificApplication\CreateApp\Infrastructure\Controller;
 
 use App\Http\Controllers\Controller;
+use Domain\SomeSpecificApplication\CreateApp\Controller\CreateAppControllerInterface;
 use Domain\SomeSpecificApplication\CreateApp\UseCase\CreateAppParameter;
 use Domain\SomeSpecificApplication\CreateApp\UseCase\CreateAppUseCase;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class CreateAppController extends Controller
+class CreateAppController extends Controller implements CreateAppControllerInterface
 {
     /**
      * @param Request $request
-     * @param CreateAppUseCase $appUseCase
+     * @param CreateAppUseCase $createAppUseCase
+     */
+    public function __construct(
+        private Request $request,
+        private CreateAppUseCase $createAppUseCase
+    ) {
+    }
+
+    /**
+     * @param
      * @return JsonResponse
      */
-    public function __invoke(
-        Request $request,
-        CreateAppUseCase $appUseCase
-    ): JsonResponse {
-
-        $request->validate([
+    public function __invoke(): JsonResponse
+    {
+        $this->request->validate([
             'user_name' => ['required', 'string', 'max:255'],
             'user_age' => ['required', 'integer', 'min:0', 'max:40'],
         ]);
 
         $parameter = new CreateAppParameter([
-            'name' => $request->input('user_name'),
-            'age' => $request->input('user_age'),
+            'name' => $this->request->input('user_name'),
+            'age' => $this->request->input('user_age'),
         ]);
 
-        $response = $appUseCase($parameter);
+        $response = $this->createAppUseCase->__invoke($parameter);
 
         return response()->json([
             'message' => 'Successfully created user!',
