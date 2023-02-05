64d5da18ffb0c6fff8daa30dee6a9d529339ebd8
tyamahori <tyamahori@gmail.com>
2023-02-05 22:28:29 +0900

    :lipstick: cs fixerの更新ディレクトリを定義

--- src/.php-cs-fixer.dist.php
+++ src/.php-cs-fixer.dist.php
@@ -3,7 +3,14 @@ declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
     ->in([
-        __DIR__,
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/package',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
     ]);
 
 return (new PhpCsFixer\Config())
